48a8987cdb209313ecd1d8f8df2c0e3b9622d439
devdrx <devdarshanpatra4@gmail.com>
2025-02-07 01:53:35 +0530

    tried synchronizing navbar to active state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
+// import { createLogger } from 'vite';
 
 const Navbar = ({navOpen}) => {
     const lastActiveLink = useRef();
     const activeBox = useRef();
+
+    const initActiveBox = () => {
+        console.log(lastActiveLink.current);
+        console.log(activeBox.current);
+        activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px';
+        activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px';
+        activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px';
+        activeBox.current.style.height = lastActiveLink.current.offsetHeight + 'px';
+    }
+
+    const activeCurrentLink = (e) => {
+        lastActiveLink.current?.classList.remove('active');
+        e.target.classList.add('active');
+        lastActiveLink.current = e.target;
+
+        activeBox.current.style.top = e.target.offsetTop + 'px';
+        activeBox.current.style.left = e.target.offsetLeft + 'px';
+        activeBox.current.style.width = e.target.offsetWidth + 'px';
+        activeBox.current.style.height = e.target.offsetHeight + 'px';
+    }
+
+    useEffect(initActiveBox , [navOpen]);
+    window.addEventListener('resize', initActiveBox);
 
     const navItems = [
     {
@@ -42,7 +66,7 @@ const Navbar = ({navOpen}) => {
                  className={className}
                  ref={ref}
                  key={index}
-                 onClick={null}
+                 onClick={activeCurrentLink}
                  >
                     {label}
                 </a>

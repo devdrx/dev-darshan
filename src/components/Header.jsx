896905a51d99b22812125d605796afe53326d9cf
devdrx <devdarshanpatra4@gmail.com>
2025-02-07 01:35:55 +0530

    added navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+
+import { useState } from 'react'
+import Navbar from './Navbar'
+
 const Header = () => {
+    const [navOpen, setNavOpen] = useState(true);
+
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900-to-zinc-900/0">
-        <div className="max-w-screen-2xl w-full mx-auto px-4">
+        <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
             <h1>
                 <a 
                 href="/" 
@@ -13,13 +19,21 @@ const Header = () => {
                     alt="Dev Darshan" />
                 </a>
             </h1>
-            <div>
-                <button className="" onClick={null}>
+            <div className="relative md:justify-self-center">
+                <button className="menu-btn md:hidden" 
+                onClick={() => setNavOpen((prev)=> !prev)}>
                     <span className="material-symbols-outlined">
                         menu
                     </span>
                 </button>
+
+                <Navbar navOpen={navOpen}/>
             </div>
+
+            <a href="#contact" 
+            className="btn btn-secondary max-md:hidden md:justify-self-end">
+                Contact Me
+            </a>
         </div>
     </header>
   )
